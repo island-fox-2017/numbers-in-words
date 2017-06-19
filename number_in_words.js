d7f7552dea76c7f553baf2c8fe6d95c435ffec02
pdvega <pdvegaa@gmail.com>
2017-06-19 16:40:31 +0700

    running untuk belasan

--- number_in_words.js
+++ number_in_words.js
@@ -1,11 +1,21 @@
 function numberToWords(number) {
   // Your code here
 
+let word = ['', 'Satu', 'Dua', 'Tiga', 'Empat', 'Lima', 'Enam', 'Tujuh', 'Delapan', 'Sembilan', 'Sepuluh', 'Sebelas'],
+  result = '';
+  
+  if(number < 12){
+    return result += word[number]
+  }else if(number < 20){
+    return result += word[number-10] + ' ' + word[11].slice(2)
+  // }else if(number < 100){
+  //   return result += 
+  // }
 
 }
 
 // Driver code
-console.log(numberToWords(1000000));
+console.log(numberToWords(12));
 
 module.exports = {
   numberToWords: numberToWords
