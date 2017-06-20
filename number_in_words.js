d3d61d15960d5e2adeb8f805679b37b5fb6e3c37
achimbaggins <achim_baggins@yahoo.com>
2017-06-20 07:40:42 +0700

    baru mulai dikerjakan

--- number_in_words.js
+++ number_in_words.js
@@ -1,5 +1,12 @@
 function numberToWords(number) {
   // Your code here
+  var deret = ['nol', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'];
+  var satuan = ['puluh', 'ratus', 'ribu', 'puluh ribu', 'ratus ribu', 'juta', 'puluh juta'];
+  if(number <= 11) {
+    return deret[number];
+  } else if(){
+    
+  }
 }
 
 // Driver code
